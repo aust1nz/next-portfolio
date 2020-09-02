c3376187d9f68d62f1784b4e4f89fa6ffa979ae8
Austin Zentz <austin.zentz@kippdc.org>
2020-09-01 23:54:56 -0400

    Update prose classes in HTML elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,21 +45,23 @@ export default function Home() {
           backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%233075b9' fill-opacity='0.35' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E")`,
         }}
       >
-        <div className="prose container flex py-8 mx-auto">
+        <div className="container flex py-8 mx-auto">
           <div className="bg-white border border-gray-500 rounded w-11/12 sm:w-7/12 mx-auto px-2 py-2 shadow">
             <h2 className="text-2xl mb-2 border-b">Who I am</h2>
-            <p>
-              For a few years after college, I taught middle school social
-              studies in schools in and near Washington, DC. It was hard! I
-              still work in education, but now I contribute behind the scenes,
-              building tools that help school systems make staff and budget
-              decisions.
-            </p>
-            <p>
-              When I'm not working, I'm usually hanging out with my wife and two
-              young kids. When they're asleep, I might be playing Overwatch on
-              the Xbox. 😃
-            </p>
+            <div className="prose">
+              <p>
+                For a few years after college, I taught middle school social
+                studies in schools in and near Washington, DC. It was hard! I
+                still work in education, but now I contribute behind the scenes,
+                building tools that help school systems make staff and budget
+                decisions.
+              </p>
+              <p>
+                When I'm not working, I'm usually hanging out with my wife and
+                two young kids. When they're asleep, I might be playing
+                Overwatch on the Xbox. 😃
+              </p>
+            </div>
           </div>
         </div>
       </div>
